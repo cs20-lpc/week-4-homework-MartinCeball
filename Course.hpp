e6951327de2dd57c14d99df9ed178a32f8d31816
MartinCeball <121062706+MartinCeball@users.noreply.github.com>
2025-09-17 23:39:57 -0700

    Update Course.hpp

--- Course.hpp
+++ Course.hpp
@@ -1,1 +1,17 @@
-// TODO:  Course class definitins go here.
+#ifndef COURSE_HPP
+#define COURSE_HPP
+
+#include <string>
+
+class Course {
+private:
+    std::string courseName;
+    std::string location;
+
+public:
+    Course(const std::string& name, const std::string& loc);
+    void display() const;
+};
+
+#endif // COURSE_HPP
+
