24be968ab4b353a68944e557ca057e72590e4408
MartinCeball <121062706+MartinCeball@users.noreply.github.com>
2025-09-17 23:40:17 -0700

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,1 +1,10 @@
-// TODO:  Course class methods implementation here.
+#include "Course.hpp"
+#include <iostream>
+
+Course::Course(const std::string& name, const std::string& loc)
+    : courseName(name), location(loc) {}
+
+void Course::display() const {
+    std::cout << courseName << " " << location;
+}
+
