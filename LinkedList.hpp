3def5061d239a5901847c2f0ce1cfdeb436532d9
MartinCeball <121062706+MartinCeball@users.noreply.github.com>
2025-09-17 23:41:22 -0700

    Update LinkedList.hpp

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,1 +1,29 @@
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
+#include "Student.hpp"
+
+class LinkedList {
+private:
+    struct Node {
+        Student studentData;
+        Node* next;
+        
+        Node(const Student& student) : studentData(student), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    LinkedList();
+    ~LinkedList();
+    void insertStudent(const Student& student);
+    bool deleteStudent(int id);
+    Student* searchStudent(int id);
+    void displayAll() const;
+    int countStudents() const;
+};
+
+#endif // LINKEDLIST_HPP
+
 
